1aa4eaaa13d5966b7b6300d9f42d9d3135f92f84
oliullahakib <oliullahakib@gmail.com>
2026-03-17 01:07:09 +0600

    solve proxy problem for production

--- src/proxy.js
+++ src/proxy.js
@@ -2,20 +2,24 @@ import { getToken } from 'next-auth/jwt'
 import { NextResponse } from 'next/server'
  
 // This function can be marked `async` if using `await` inside
-const privateRoute = ['/cart','/checkout','/profile','/profile/my-orders','/profile/reviews','/profile/wishlist']
+const privateRoute = ['/cart', '/checkout', '/profile'];
+
 export async function proxy(req) {
-    const token = await getToken({req,secret:process.env.NEXTAUTH_SECRET})
-    const reqUrl = req.nextUrl.pathname
-    const isPrivate = privateRoute.some(route=>route.startsWith(reqUrl))
-    if(!token && isPrivate){
-        return NextResponse.redirect(new URL(`/login?callbackUrl=${reqUrl}`, req.url))
+    const token = await getToken({
+        req,
+        secret: process.env.NEXTAUTH_SECRET,
+        secureCookie: process.env.NODE_ENV === 'production'
+    });
+
+    const reqUrl = req.nextUrl.pathname;
+    const isPrivate = privateRoute.some(route => reqUrl.startsWith(route));
+
+    if (!token && isPrivate) {
+        return NextResponse.redirect(new URL(`/login?callbackUrl=${reqUrl}`, req.url));
     }
-//   return NextResponse.redirect(new URL('/', request.url))
-return NextResponse.next()
+
+    return NextResponse.next();
 }
- 
-// Alternatively, you can use a default export:
-// export default function proxy(request) { ... }
  
 export const config = {
   matcher: ['/cart/:path*','/checkout/:path*','/profile/:path*'],
